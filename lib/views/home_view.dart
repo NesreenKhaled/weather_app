b025c43d618cb1effc7080fe887ce3f6adb0802d
NesreenKhaled <nsreenkhaled135@gmail.com>
2025-09-05 22:22:11 +0300

    integrate bloc to home views

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:weather_app/widgets/no_weather_body.dart';
 import 'package:weather_app/views/search_view.dart';
 import 'package:weather_app/widgets/weather_info_body.dart';
+import 'package:weather_app/cubits/get_weather_cubit/get_weather_cubit.dart';
+import 'package:weather_app/cubits/get_weather_cubit/get_weather_states.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({Key? key}) : super(key: key);
@@ -26,9 +29,19 @@ class HomeView extends StatelessWidget {
           ),
         ],
       ),
-      body:
-           const NoWeatherBody()
-          
+      body: BlocBuilder<GetWeatherCubit, Weatherstate>(
+        builder: (context, state) {
+          if (state is Weatherloadedstate) {
+            return WeatherInfoBody();
+          } else if (state is Weatherfailurstate) {
+            return Center(
+              child: Text('Failed to load weather data. Please try again.'),
+            );
+          } else {
+            return NoWeatherBody();
+          }
+        },
+      ),
     );
   }
 }
